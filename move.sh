a9610b2fae57644bae8fb3880ba3ce26c99b3614
0dB <0dB@users.noreply.github.com>
2022-08-08 09:41:12 +0200

    move.sh now works correctly with path names with spaces and only for expected files.

--- move.sh
+++ move.sh
@@ -1,22 +1,19 @@
-for f in ./*.md; do
+for f in ./20[0-9][0-9]-[0-1][0-9]-[0-3][0-9]T[0-9][0-9]-*.md; do
+     prefix='Day One Export'
+     mkdir -p "${prefix}"
      # remove leading ./
+     # You cannot have spaces around your = in shell variable assignments.
      d=${f#./}
      # get YYYY and make directory (if it does not exist)
      Y=${d%%-*} # removes everything after first '-'
-     mkdir -p $Y
+     mkdir -p "${prefix}/$Y"
      # get YYYY-MM and make directory (if it does not exist).
      # This can probably be done with one line instead of two.
      YMX=${d%-*} # removes everything after last '-'
      YM=${YMX%-*} # one more time, now we have YYYY-MM
      # move YYYY-MM into YYYY folder
      folder=$Y/$YM
-     mkdir -p $folder
+     mkdir -p "${prefix}/${folder}"
      # move file into YYYY/YYYY-MM folder
-     # use foo="$foo"' world'?
-     # put double quotes around variable substitutions and command substitutions?
-     # word1=’ball’; word2=’park’; compound="$word1$word2″
-     # Single quotes are "hard" in the shell, meaning that things like $… and ` (backtick expressions)
-     # are not interpolated into them.
-     # You cannot have spaces around your = in shell variable assignments.
-     mv $f $folder
+     mv $f "${prefix}/$folder"
 done
